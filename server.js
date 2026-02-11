98c22b1e1b2fe9ec632bcc85b9c7493d60edf564
taibarrett1-netizen <tai.barrett1@gmail.com>
2026-02-11 14:59:04 +0000

    Update server.js

--- server.js
+++ server.js
@@ -299,6 +299,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Dashboard at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Dashboard at http://0.0.0.0:${PORT}`);
 });
